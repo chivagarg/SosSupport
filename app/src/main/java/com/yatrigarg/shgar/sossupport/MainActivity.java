37a83cac2321066a5a6db808976e4793b76c6260
chivagarg <chivagarg@gmail.com>
2015-06-28 21:42:03 -0700

    Adding parse and data push from Client

--- app/src/main/java/com/yatrigarg/shgar/sossupport/MainActivity.java
+++ app/src/main/java/com/yatrigarg/shgar/sossupport/MainActivity.java
@@ -9,12 +9,16 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.Parse;
+
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.initialize(this, "A5Zs7r9hVA05tzRfMhvL62r8jQrNUDN1BHX61nzE", "8Mii6JsnI3AbpDFM6vk6xo2pNxQ5cZatoOXl0UnU");
     }
 
     public void SendMail(View v) throws Exception
--- app/src/main/java/com/yatrigarg/shgar/sossupport/asyncSend.java
+++ app/src/main/java/com/yatrigarg/shgar/sossupport/asyncSend.java
@@ -9,6 +9,7 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.parse.ParseObject;
 import com.sun.mail.smtp.SMTPTransport;
 
 import javax.mail.Message;
@@ -36,26 +37,32 @@ public class asyncSend extends AsyncTask<String, Void, Void>{
     protected Void doInBackground(String... params) {
         Mail m = new Mail("postmaster@sandbox05ba2b4a8ba5489bb94ee9ec48fcd2f1.mailgun.org", "08137d2220452144f7eac66702922e2d");
         m.setHost("smtp.mailgun.org");
-        m.setFrom(params[0]);
-        //String[] toArr = {"chivagarg@gmail.com"};
-        //String[] toArr = {"we_can_help@cable.comcast.com"};
-        //String[] toArr = {"attcustomercare@att.com"};
+
+        String sender = params[0];
+        String recipient = params[1];
+        String subject = params[2];
+        String body = params[3];
+
+        m.setFrom(sender);
         String[] toArr = new String[1];
-        toArr[0] = params[1];
+        toArr[0] = recipient;
         m.setTo(toArr);
-        //m.setFrom("blahblah@shivasapp.com");
-       // m.setFrom("postmaster@sandbox05ba2b4a8ba5489bb94ee9ec48fcd2f1.mailgun.org");
-        m.setSubject(params[2]);
-        m.setBody(params[3]);
-        //m.setSubject("Is this working?");
-        //m.setBody("Are things broken?");
-       // m.setPort("587");
+        m.setSubject(subject);
+        m.setBody(body);
 
         try {
             // m.addAttachment("/sdcard/filelocation");
 
             if(m.send()) {
                 success = true;
+                ParseObject message = new ParseObject("Messages");
+                message.put("Recipient", recipient);
+                message.put("Sender", sender);
+                message.put("Subject", subject);
+                message.put("MailBody", body);
+                message.put("RequestType", "Client Push");
+                message.saveInBackground();
+
                 //Toast.makeText(asyncSend.this, "Email was sent successfully.", Toast.LENGTH_LONG).show();
             } else {
                // Toast.makeText(MainActivity.this, "Email was not sent.", Toast.LENGTH_LONG).show();
